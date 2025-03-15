1ac20478cf6a3fd1384938942f466a03ba17eed2
Yuk Fang <yuk@YUKMBAIR.local>
2025-03-15 22:32:43 +0800

    xx

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
-const PORT = 3039;
+const PORT = process.env.PORT || 3039;
 
 export default defineConfig({
   build: {
@@ -20,7 +20,7 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 490,
+    chunkSizeWarningLimit: 1000,
   },
   plugins: [
     react(),
