f58db55a9b79262936598741c5aead8580bdfbb5
Yuk Fang <yuk@YUKMBAIR.local>
2025-03-15 21:56:53 +0800

    init

---
+++ src/layouts/config-nav-workspace.tsx
@@ -0,0 +1,22 @@
+// ----------------------------------------------------------------------
+
+export const _workspaces = [
+  {
+    id: 'bc-1',
+    name: 'BC 1',
+    logo: `/assets/icons/workspaces/logo-1.webp`,
+    plan: 'Entertainment',
+  },
+  {
+    id: 'bc-2',
+    name: 'BC 2',
+    logo: `/assets/icons/workspaces/logo-2.webp`,
+    plan: 'Entertainment',
+  },
+  {
+    id: 'bc-3',
+    name: 'BC 3',
+    logo: `/assets/icons/workspaces/logo-3.webp`,
+    plan: 'Hotel',
+  },
+];
